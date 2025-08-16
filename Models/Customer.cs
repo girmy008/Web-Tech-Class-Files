116982500caf49ffbf5caa8073e506c22ed54349
Mittens <hello@mittens.dev>
2025-08-17 08:51:30 +0930

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AOWebApp.Models;
+
+public partial class Customer
+{
+    public int CustomerId { get; set; }
+
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = null!;
+
+    [NotMapped]
+    [Display(Name = "Customer Name")]
+    public string FullName { get => $"{FirstName} {LastName}"; }
+
+    [NotMapped]
+    [Display(Name = "Contact Number")]
+    public string ContactNumber { get =>
+            string.IsNullOrWhiteSpace(SecondaryPhoneNumber) ? MainPhoneNumber :
+            string.IsNullOrWhiteSpace(MainPhoneNumber) ? SecondaryPhoneNumber : $"{MainPhoneNumber}<br/>{SecondaryPhoneNumber}";
+    }
+
+
+    [Display(Name = "Email")]
+    public string Email { get; set; } = null!;
+
+    public string MainPhoneNumber { get; set; } = null!;
+
+    public string? SecondaryPhoneNumber { get; set; }
+
+    public int AddressId { get; set; }
+
+    public virtual Address? Address { get; set; } = null!;
+
+    public virtual ICollection<CustomerOrder> CustomerOrders { get; set; } = new List<CustomerOrder>();
+
+    public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+}
---
+++ ViewModel/CustomerSearchVM.cs
@@ -0,0 +1,19 @@
+﻿using AOWebApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace AOWebApp.ViewModel
+{
+    public class CustomerSearchVM
+    {
+        [Required(ErrorMessage ="You must provide a Customer Name")]
+        public string SearchText { get; set; }
+        public string Suburb { get; set; }
+
+        public SelectList SuburbSelectList { get; set; }
+
+        public IEnumerable<Customer>? CustomerList { get; set; }
+
+
+    }
+}
