4f8d76acaf992eda966d6c63b69d10502a31a9e3
girmy008 <164983725+girmy008@users.noreply.github.com>
2025-10-13 15:39:36 +1030

    update

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,20 @@
+﻿using AOWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AOWebApp.Controllers
+{
+    public class ReportsController : Controller
+    {
+        DbContext _context;
+        public IActionResult Index()
+        {
+            return View("AnnualSalesReport");
+        }
+        public ReportsController(AmazonDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
